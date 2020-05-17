3256be3ba8b9d9950a0f4e4ca6621efef1b00a96
sanjibbehera <51856169+sanjibbehera@users.noreply.github.com>
2020-05-18 02:11:44 +0530

    Update setup-hosts.sh

--- rhel8/setup-hosts.sh
+++ rhel8/setup-hosts.sh
@@ -3,7 +3,7 @@ set -e
 IFNAME=$1
 ADDRESS="$(ip -4 addr show $IFNAME | grep "inet" | head -1 |awk '{print $2}' | cut -d/ -f1)"
 sed -e "s/^.*${HOSTNAME}.*/${ADDRESS} ${HOSTNAME} ${HOSTNAME}.local/" -i /etc/hosts
-
+sudo setenforce 0 && sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
 sudo yum install -y wget net-tools
 
 # Update /etc/hosts about other hosts
